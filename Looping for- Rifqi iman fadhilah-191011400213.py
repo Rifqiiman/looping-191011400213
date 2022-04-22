168cabc1350ce8d93b443e9c5046cbf29c5ead6a
Rifqiiman <104218543+Rifqiiman@users.noreply.github.com>
2022-04-22 23:19:52 +0700

    Add files via upload

---
+++ Looping for- Rifqi iman fadhilah-191011400213.py
@@ -0,0 +1,15 @@
+#nilai ganjil dan genap
+a = 2
+print ("Genap :")
+for a in range (1,21):
+    if a%2==0:
+        print (a)
+        a=a+1
+print(" ")
+print("Ganjil :")        
+a=0
+for a in range (1,21):
+    if a%2!=0:
+        print (a)
+        a=a+1
+            
